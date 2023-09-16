9b70a14d0b07126d4f87fb83991d072c0620a922
Tom-the-Bomb <thello522@gmail.com>
2023-09-16 17:07:53 -0400

    minor fix

--- src/components/Triangle.jsx
+++ src/components/Triangle.jsx
@@ -4,14 +4,19 @@ import Tooltip from "./Tooltip";
 function Number({n, k, conditions}) {
     const number = nk(n, k);
     const hl = conditions.filter((o) => o.f(number, n, k))[0];
+
+    const inner =
+        <div style={hl ? {backgroundColor: document.getElementById(hl.name + "color").value} : null}
+            className="d-flex justify-content-center align-items-center p-2 rounded-2 number"
+        >
+            {number}
+        </div>;
     return (
-        <Tooltip placement="top" text={number === 1 ? 1 : `${nk(n - 1, k - 1)} + ${nk(n - 1, k)}`}>
-            <div style={hl ? {backgroundColor: document.getElementById(hl.name + "color").value} : null}
-                className="d-flex justify-content-center align-items-center p-2 rounded-2 number"
-            >
-                {number}
-            </div>
-        </Tooltip>
+        number === 1
+            ? inner
+            : <Tooltip placement="top" text={number === 1 ? 1 : `${nk(n - 1, k - 1)} + ${nk(n - 1, k)}`}>
+                {inner}
+            </Tooltip>
     )
 }
 
