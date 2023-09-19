19776c483f1c00cafc321909a4bd6bb7b848eb4d
Tom-the-Bomb <thello522@gmail.com>
2023-09-18 22:31:11 -0400

    fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     return (
         <footer className="text-center card-footer p-4 mt-5 footer">
             Made by <a
-                className="text-white-50 footer-link"
+                className="text-white-50"
                 target="_blank" rel="noreferrer"
                 href="https://github.com/Tom-the-Bomb"
             >Tom-the-Bomb</a> 2023
